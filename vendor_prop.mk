d5ed791b078fbe7d3cc3292b60a3bc7d64b97d5e
Lunarixus <Nathan@lunarixus.party>
2020-09-20 02:30:12 +0000

    msm8998-common: Add required GPU props
    These tell the ROM to use the correct Adreno drivers which fixes graphical artifacts.
    
    Credit to Sultan for the triple buffers prop.
    
    Credit to kdrag0n for noticing vulkan prop was missing.
    
    Change-Id: I3481092f3aea2be2091c8c64ca04c426a244a696

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -191,6 +191,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.egl=adreno \
+    ro.hardware.vulkan=adreno \
     ro.opengles.version=196610 \
     debug.sf.hw=1 \
     debug.sf.enable_hwc_vds=1 \
@@ -277,6 +279,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # SurfaceFlinger
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.protected_contents=true \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
