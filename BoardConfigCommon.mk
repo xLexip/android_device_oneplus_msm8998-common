2ad694c862d8e69a739778fa220ce3a67aff0d0a
pavanpaps <pavanishot200@gmail.com>
2020-11-21 23:53:27 +0530

    msm8998-common: Switch to lazy kernel
    Kernel Source: https://github.com/ederekun/lazy_kernel_op5-t/tree/X-alt

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -73,7 +73,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8998
-TARGET_KERNEL_CONFIG := lineage_oneplus5_defconfig
+TARGET_KERNEL_CONFIG := lazy_defconfig
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
