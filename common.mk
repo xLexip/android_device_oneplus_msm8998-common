2b35867a5b123253cce02c0dc78d2b3bbb8a56ee
Anirudh Gupta <anirudhgupta109@gmail.com>
2020-11-21 23:48:54 +0530

    [SQUASH] msm8998-common: DeviceSettings - Imported from https://github.com/AOSiP-Devices/device_oneplus_sdm845-common/tree/f55c0218951d9b0f6586bdf5041a64ea14c96460/DeviceSettings - Adapted to AndroidX - Includes all translations from pie cycle - Dropped DC-Dimming QS Tile
    NurKeinNeid edits:
    - Modify and adapt to msm8998-common
    
    pavanpaps edits:
    - Rename from derp.device to oneplus.extras inspired from https://github.com/ShapeShiftOS-Devices/device_oneplus_sdm845-common/commit/b4875e07a6a8b1553ad40e67a87ad3f578d2c4dd
    - Adapt to Evolution X's FWB for TouchScreenGestureSettings
    - Some small edits here and there
    
    Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Co-authored-by: Daniel Solomon <solomondaniel@google.com>
    Co-authored-by: Luca Stefani <luca.stefani.ge1@gmail.com>
    Co-authored-by: darkobas <darkobas@gmail.com>
    Co-authored-by: Max Weninger <max.weninger@gmail.com>
    Co-authored-by: Vachounet <vachounet@live.fr>
    Co-authored-by: kldoc <fred.berbigier@gmail.com>
    Co-authored-by: Christian Oder <myself5@carbonrom.org>
    Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
    Co-authored-by: Joey Yu <xiaowei.yu@dawsoncollege.qc.ca>
    Co-authored-by: gotenksIN <forumomkar@gmail.com>
    Co-authored-by: idoybh <idoybh2@gmail.com>
    Co-authored-by: NurKeinNeid <mralexman3000@gmail.com>
    Co-authored-by: Ashwin R C <ashwin2001achu@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: AshutoshSundresh <ashutoshsundresh@gmail.com>
    Co-authored-by: MASTERGUY <murtipipo@gmail.com>

--- common.mk
+++ common.mk
@@ -164,6 +164,10 @@ PRODUCT_PACKAGES += \
     com.quicinc.cne \
     services-ext
 
+# Device Settings
+PRODUCT_PACKAGES += \
+    DeviceSettings
+
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
