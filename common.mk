c4977d9c3376636fd156d3f0a583903630ac0d04
xLexip <lexip.ctrl@gmail.com>
2020-11-26 15:23:26 +0100

    msm8998-common: Nuke audio_amplifier

--- common.mk
+++ common.mk
@@ -95,7 +95,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
-    audio_amplifier.msm8998 \
     audio.primary.msm8998 \
     audio.r_submix.default \
     audio.usb.default \
