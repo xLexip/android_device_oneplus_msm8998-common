4cf41583be6da1c60fbd778d1cecf5f56707919d
Bruno Martins <bgcngm@gmail.com>
2020-12-08 17:14:02 +0100

    msm8998-common: doze: Add AOD Support to Ambient Display
    * This makes the always on display mode feature visible for those
       devices that explicitly set it as available via AOSP overlay.
       Should only be enabled on devices where the display has been tuned
       to be power efficient in DOZE and/or DOZE_SUSPEND states.

--- doze/src/org/lineageos/settings/doze/Utils.java
+++ doze/src/org/lineageos/settings/doze/Utils.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2015 The CyanogenMod Project
- *               2017-2018 The LineageOS Project
+ *               2017-2020 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,16 +25,20 @@ import androidx.preference.PreferenceManager;
 import android.provider.Settings;
 import android.util.Log;
 
-import androidx.preference.PreferenceManager;
+import com.android.internal.hardware.AmbientDisplayConfiguration;
 
+import static android.provider.Settings.Secure.DOZE_ALWAYS_ON;
 import static android.provider.Settings.Secure.DOZE_ENABLED;
 
 public final class Utils {
 
+    protected static final String ALWAYS_ON_DISPLAY = "always_on_display";
+    protected static final String CATEG_PICKUP_SENSOR = "pickup_sensor";
     protected static final String CATEG_PROX_SENSOR = "proximity_sensor";
     protected static final String GESTURE_PICK_UP_KEY = "gesture_pick_up";
     protected static final String GESTURE_HAND_WAVE_KEY = "gesture_hand_wave";
     protected static final String GESTURE_POCKET_KEY = "gesture_pocket";
+
     private static final String TAG = "DozeUtils";
     private static final boolean DEBUG = false;
     private static final String DOZE_INTENT = "com.android.systemui.doze.pulse";
@@ -52,7 +56,7 @@ public final class Utils {
     }
 
     protected static void checkDozeService(Context context) {
-        if (isDozeEnabled(context) && sensorsEnabled(context)) {
+      if (isDozeEnabled(context) && !isAlwaysOnEnabled(context) && sensorsEnabled(context)) {
             startService(context);
         } else {
             stopService(context);
@@ -106,5 +110,17 @@ public final class Utils {
     protected static boolean sensorsEnabled(Context context) {
         return isPickUpEnabled(context) || isHandwaveGestureEnabled(context)
                 || isPocketGestureEnabled(context);
+    }
+
+    protected static boolean enableAlwaysOn(Context context, boolean enable) {
+      return Settings.Secure.putIntForUser(context.getContentResolver(),DOZE_ALWAYS_ON, enable ? 1 : 0, UserHandle.USER_CURRENT);
+    }
+
+    private static boolean isAlwaysOnEnabled(Context context) {
+      return Settings.Secure.getIntForUser(context.getContentResolver(),DOZE_ALWAYS_ON, 1, UserHandle.USER_CURRENT) != 0;
+    }
+
+    protected static boolean alwaysOnDisplayAvailable(Context context) {
+      return new AmbientDisplayConfiguration(context).alwaysOnAvailable();
     }
 }
