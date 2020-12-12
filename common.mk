90811d3e92a4dded8229c965e18c725d93f6af7c
Lexip <lexip.ctrl@gmail.com>
2020-12-12 22:37:20 +0100

    msm8998-common: Nuke Pixel Power HAL
    Reason: Doze / DoubleTapWake Issues
    
    This - among other things - reverts commit 91b90a6e72cd91008b0710add248779e452fbc1c.

--- common.mk
+++ common.mk
@@ -390,10 +390,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-		android.hardware.power@1.3-service.pixel-libperfmgr
-
-PRODUCT_COPY_FILES += \
-		$(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    android.hardware.power-service-qti
 
 # Protobuf
 PRODUCT_PACKAGES += \
