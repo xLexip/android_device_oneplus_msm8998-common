91b90a6e72cd91008b0710add248779e452fbc1c
Lexip <lexip.ctrl@gmail.com>
2020-12-12 15:28:48 +0100

    msm8998-common: Enable Pixel Power HAL

--- common.mk
+++ common.mk
@@ -390,7 +390,10 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+		android.hardware.power@1.3-service.pixel-libperfmgr
+
+PRODUCT_COPY_FILES += \
+		$(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Protobuf
 PRODUCT_PACKAGES += \
