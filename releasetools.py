18f59eae6890b8511c459c403557b5a997c2f357
xLexip <lexip.ctrl@gmail.com>
2021-04-08 22:27:27 +0200

    msm8998-common: Fix typo in releasetools

--- releasetools.py
+++ releasetools.py
@@ -17,7 +17,7 @@ def OTA_MaintainerSection(info):
     info.script.AppendExtra('ui_print("  / /___ ");')
     info.script.AppendExtra('ui_print("  \____/ ");')
     info.script.AppendExtra('ui_print("                                             -");')
-    info.script.AppendExtra('ui_print("Pixel Exeprience 11+ // OnePlus 5 Series");')
+    info.script.AppendExtra('ui_print("Pixel Experience 11+ // OnePlus 5 Series");')
     info.script.AppendExtra('ui_print("Maintained by xLexip");')
     info.script.AppendExtra('ui_print("                                             -");')
     info.script.AppendExtra('ui_print("Get help and important information on Telegram");')
@@ -28,4 +28,3 @@ def OTA_MaintainerSection(info):
     info.script.AppendExtra('ui_print("github.com/xlexip/pe");')
     info.script.AppendExtra('ui_print("                                             -");')
     info.script.AppendExtra('ui_print("Have fun with Pixel Experience! :]");')
-    info.script.AppendExtra('ui_print("                                             -");')
