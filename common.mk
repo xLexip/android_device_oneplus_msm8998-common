fd42dc8b0d552e60484502ea453b01b725440264
xLexip <x@lexip.dev>
2021-05-12 17:18:01 +0200

    msm8998-common: Drop pocketmode
    * In favour of rom-side implementation in PixelExperience.

--- common.mk
+++ common.mk
@@ -195,8 +195,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service \
-    OneplusPocketMode
+    android.hardware.biometrics.fingerprint@2.1-service
 
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
