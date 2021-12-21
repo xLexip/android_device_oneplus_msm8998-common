ddf9788447ceb07f9c6491f62df102b3281ae11d
xLexip <x@lexip.dev>
2021-12-21 19:24:00 +0100

    msm8998-common: DeviceSettings: Partly reimport the Device Settings from eleven
    * Cleanup, commit description and credits will follow!#
    
    Signed-off-by: xLexip <x@lexip.dev>

--- common.mk
+++ common.mk
@@ -168,6 +168,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
+# Device Settings
+PRODUCT_PACKAGES += \
+    DeviceSettings
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
