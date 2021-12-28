6b90a6bfd4b2e077b9bd92b556bf377d35a585e7
xLexip <x@lexip.dev>
2021-12-28 23:10:14 +0100

    (TMP) msm8998-common: selinux: Build permissive
    Signed-off-by: xLexip <x@lexip.dev>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -59,7 +59,7 @@ BUILD_BROKEN_DUP_RULES := true
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=2048
 BOARD_KERNEL_CMDLINE += loop.max_part=7
-#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
