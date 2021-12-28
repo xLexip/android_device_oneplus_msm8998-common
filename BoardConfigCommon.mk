f0ce82371b9518d3438f48762ba49d8f3791b04b
xLexip <x@lexip.dev>
2021-12-28 23:10:14 +0100

    msm8998-common: selinux: Build enforcing
    This reverts commit 393925339299f9021276ce92135fbdd335f40e15.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -56,10 +56,10 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a73
 BUILD_BROKEN_DUP_RULES := true
 
 # Kernel
+# BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=2048
 BOARD_KERNEL_CMDLINE += loop.max_part=7
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
